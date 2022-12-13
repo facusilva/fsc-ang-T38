c29f88b2d5fb94309ec41170a6a907de551d8016
facusilva <facun772@gmail.com>
2022-12-13 10:03:17 +0100

    añadidos servicios

--- src/app/character/character.component.ts
+++ src/app/character/character.component.ts
@@ -1,22 +1,22 @@
 import { HttpClient } from '@angular/common/http';
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { RestService } from '../rest.service';
 
 @Component({
   selector: 'app-character',
   templateUrl: './character.component.html',
   styleUrls: ['./character.component.css']
 })
-export class CharacterComponent {
+export class CharacterComponent implements OnInit{
 
-  constructor(private route: ActivatedRoute, private http: HttpClient) {}
+  constructor(private route: ActivatedRoute, private RestService:RestService) {}
 
   character:any;
 
   ngOnInit(): void {
     const id = this.route.snapshot.paramMap.get('id');
-
-    this.character = this.http.get("https://rickandmortyapi.com/api/character/"+id).subscribe( result => this.character = result );
+    this.RestService.getCharacters(`https://rickandmortyapi.com/api/character/`+id).subscribe( result => this.character = result );
 
 }
 
--- src/app/characters/characters.component.ts
+++ src/app/characters/characters.component.ts
@@ -1,15 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { RestService } from '../rest.service';
 
 @Component({
   selector: 'app-characters',
   templateUrl: './characters.component.html',
   styleUrls: ['./characters.component.css']
 })
-export class CharactersComponent {
+export class CharactersComponent implements OnInit{
 
-  constructor(private http: HttpClient) {}
+  constructor(private RestService:RestService) {}
 
-  characters:any = this.http.get("https://rickandmortyapi.com/api/character/1,2,3,4,5").subscribe( result => this.characters = result );
+  characters:any;
+
+  ngOnInit():void{
+    this.RestService.getCharacters(`https://rickandmortyapi.com/api/character/1,2,3,4,5`).subscribe( result => this.characters = result );
+  }
 
 }
